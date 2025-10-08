59bd59956ca70aab53bd19bc83ad1b09cc4fde62
Gabriel Santos Costa <138066007+CostaX549@users.noreply.github.com>
2025-10-08 08:03:52 -0300

    Update page.jsx

--- app/(main)/video-call/page.jsx
+++ app/(main)/video-call/page.jsx
@@ -1,7 +1,7 @@
 import VideoCall from "./video-call-ui";
 
 export default async function VideoCallPage({ searchParams }) {
-  const { sessionId, token } = await searchParams;
+  const { sessionId, token, chatId } = await searchParams;
 
-  return <VideoCall sessionId={sessionId} token={token} />;
+  return <VideoCall sessionId={sessionId} token={token} chatId={chatId} />;
 }
